3a1df5e03677c26b326bf0e8c44fbb01f0b7b358
BEN EL HADJ Abir <abir.behadj@gmail.com>
2025-06-14 03:58:48 +0200

    mise a jour script

--- renouvellement_certificat.sh
+++ renouvellement_certificat.sh
@@ -4,13 +4,16 @@
 DOMAIN="bhkbtp.fr"
 PORT=443
 
+# === Se positionner dans le dossier ou se trouve ce sripte ===
+cd "$(dirname "$0")"
+
 # === Récupération de la date d'expiration ===
 CERT_DATE=$(echo | openssl s_client -servername "$DOMAIN" -connect "$DOMAIN:$PORT" 2>/dev/null \
   | openssl x509 -noout -enddate \
   | cut -d= -f2)
 
 if [ -z "$CERT_DATE" ]; then
-  echo "Erreur : impossible de récupérer le certificat pour $DOMAIN"
+  echo "`date` : Erreur : impossible de récupérer le certificat pour $DOMAIN"
   exit 1
 fi
 
@@ -19,9 +22,9 @@ CERT_EXPIRY_TS=$(date -d "$CERT_DATE" +%Y-%m-%d)
 TOMORROW_TS=$(date -d "tomorrow" +%Y-%m-%d)
 
 # === Comparaison ===
-if [ "$CERT_EXPIRY_TS" -eq "$TOMORROW_TS" ]; then
-  echo "🔔 Le certificat de $DOMAIN expire demain. Renouvellement en cours..."
+if [ "$CERT_EXPIRY_TS" == "$TOMORROW_TS" ]; then
+  echo "`date` : 🔔 Le certificat de $DOMAIN expire demain. Renouvellement en cours..."
   docker compose run --rm certbot renew
 else
-  echo "Le certificat de $DOMAIN expire le : $CERT_EXPIRY_TS"
+  echo "`date` : Le certificat de $DOMAIN expire le : $CERT_EXPIRY_TS"
 fi
